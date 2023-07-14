02a999bb2947096aa170e5325ecc625e4a6dd479
Newton Bujiku <bujikun@vodacom.co.tz>
2023-07-14 13:34:21 +0300

    Add project files

---
+++ src/test/java/com/bujikun/fsd/capstone/eHealthcare/EHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujikun.fsd.capstone.eHealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
